25aa3e8567c2de027b558934a0db8cbf901b34cd
raufer <raufer92@gmail.com>
2020-12-14 13:22:46 +0000

    loaded model bug

--- src/io/model.py
+++ src/io/model.py
@@ -1,16 +1,13 @@
 from src import device
-from src.models.roberta_classifier import ROBERTAClassifier
 from src.utils.checkpoints import load_checkpoint
 
 
-def load_model(output_path: str) -> ROBERTAClassifier:
+def load_model(model, output_path: str):
     """
     Loads the model in `output_path` and returns it
     in the correct device
     """
-    model = ROBERTAClassifier()
     model = model.to(device)
-
     load_checkpoint(output_path + '/model.pkl', model)
     return model
 
