07fad949e38ad66983b49889582c4f9c2e3dab09
raufer <raufer92@gmail.com>
2021-01-25 17:39:59 +0000

    prediciton directly on texts

--- src/ops/prediction.py
+++ src/ops/prediction.py
@@ -1,3 +1,4 @@
+import logging
 import torch
 
 from sklearn.metrics import f1_score
@@ -7,6 +8,10 @@ from typing import List
 from typing import Tuple
 
 from src import device
+from src.constants import BATCH_SIZE
+from src.data.simple_text_dataset import SimpleTextDataset
+
+logger = logging.getLogger(__name__)
 
 
 def predict(model, iterator: DataLoader) -> Tuple[List, List]:
@@ -33,5 +38,38 @@ def predict(model, iterator: DataLoader) -> Tuple[List, List]:
             y_probs.extend(sm(output).tolist())
 
     return y_pred, y_probs
+
+
+def predict_texts(model, tokenizer, texts: List[str]) -> Tuple[List[int], List[Tuple[float, float]]]:
+    """
+    Returns a model inference for each text entry in `texts`
+
+    * List with the index of the predicted class
+    * List with the normalized of all classes (normalized)
+    """
+    acc_y_preds = []
+    acc_y_probs = []
+
+    inference_batch_size = 20000
+
+    n = (len(texts) // inference_batch_size) + 1
+
+    for i in range(n):
+        a = i * inference_batch_size
+        b = (i + 1) * inference_batch_size
+
+        logger.info(f"Inference batch: '{a}' to '{b}'. Batch '{i}/{n}'")
+
+        working_texts = texts[a:b]
+
+        dataset = SimpleTextDataset(working_texts, tokenizer)
+        iterator = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=0)
+
+        y_pred, y_probs = predict(model, iterator)
+
+        acc_y_preds.extend(y_pred)
+        acc_y_probs.extend(y_probs)
+
+    return acc_y_preds, acc_y_probs
 
 
